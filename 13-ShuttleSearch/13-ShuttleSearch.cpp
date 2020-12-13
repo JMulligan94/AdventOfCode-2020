69f0b4214cc909b897cb1941e1c5c6be1f82d031
JMulligan94 <j.mulligan94@hotmail.co.uk>
2020-12-13 11:08:04 +0000

    Day 13 - Shuttle Search - WIP

---
+++ 13-ShuttleSearch/13-ShuttleSearch.cpp
@@ -0,0 +1,23 @@
+// 13-ShuttleSearch.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	ifstream inputStream("input.txt");
+	//ifstream inputStream("test.txt");
+	if (inputStream.is_open())
+	{
+		string line;
+		while (getline(inputStream, line))
+		{
+
+		}
+	}
+}
