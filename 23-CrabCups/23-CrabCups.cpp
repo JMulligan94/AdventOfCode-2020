fa229e07a15abf694c089a964f165d48a96ed7ec
JMulligan94 <j.mulligan94@hotmail.co.uk>
2021-01-02 04:12:48 +0000

    Day 23 - Crab Cups - DONE!

--- 23-CrabCups/23-CrabCups.cpp
+++ 23-CrabCups/23-CrabCups.cpp
@@ -9,7 +9,8 @@
 #include <stdint.h>
 #include <cassert>
 
-//#define DEBUG_PRINT 1
+//#define DEBUG_PRINT
+//#define DEBUG_PRINT_CUPS
 
 using namespace std;
 
@@ -17,8 +18,6 @@ class Cup
 {
 public:
 	int m_label;
-
-	Cup* m_prev;
 	Cup* m_next;
 };
 
@@ -27,32 +26,43 @@ class CupGame
 public:
 	CupGame(string input)
 	{
-		const int cupCount = (int)input.size();
+		m_numCups = (int)input.size();
 		m_lowestLabel = INT_MAX;
 		m_highestLabel = INT_MIN;
+		vector<Cup*> cupsAdded;
 
-		for (int i = 0; i < cupCount; ++i)
+		for (int i = 0; i < m_numCups; ++i)
 		{
 			int label = input[i] - '0';
 			Cup* cup = new Cup();
 			cup->m_label = label;
-			m_allCups.push_back(cup);
+
+			if (label >= (int)m_allCups.size())
+				m_allCups.resize(label + 1);
+
+			m_allCups[label] = cup;
+			cupsAdded.push_back(cup);
 
 			m_lowestLabel = min(m_lowestLabel, label);
 			m_highestLabel = max(m_highestLabel, label);
 		}
 
-		for (int i = 0; i < cupCount - 1; ++i)
+		for (int i = 0; i < (int)cupsAdded.size() - 1; ++i)
 		{
-			ConnectCup(m_allCups[i], m_allCups[i + 1]);
+			cupsAdded[i]->m_next = cupsAdded[i+1];
 		}
-		ConnectCup(m_allCups.back(), m_allCups.front());
+
+		m_firstCup = cupsAdded.front();
+		m_lastCup = cupsAdded.back();
+		m_lastCup->m_next = m_firstCup;
 	}
 
 	~CupGame()
 	{
 		for (int i = 0; i < (int)m_allCups.size(); ++i)
 		{
+			if (m_allCups[i] == nullptr)
+				continue;
 			delete m_allCups[i];
 			m_allCups[i] = nullptr;
 		}
@@ -61,7 +71,7 @@ public:
 
 	void PrintCups(Cup* startCup)
 	{
-#ifdef DEBUG_PRINT
+#ifdef DEBUG_PRINT_CUPS
 		cout << "cups:  (" << startCup->m_label << ")  ";
 		Cup* cup = startCup->m_next;
 		while (cup != startCup)
@@ -72,54 +82,23 @@ public:
 #endif
 	}
 
-	void ConnectCup(Cup* cupA, Cup* cupB)
-	{
-		cupA->m_next = cupB;
-		cupB->m_prev = cupA;
-	}
-
-	void InsertCup(Cup* insertionCup, Cup* prev)
-	{
-		assert(insertionCup->m_next == nullptr 
-			&& insertionCup->m_prev == nullptr);
-
-		Cup* nextCup = prev->m_next;
-		
-		prev->m_next = insertionCup;
-		insertionCup->m_prev = prev;
-
-		insertionCup->m_next = nextCup;
-		nextCup->m_prev = insertionCup;
-	}
-
-	void RemoveCup(Cup* cupToRemove)
-	{
-		assert(cupToRemove->m_next != nullptr
-			&& cupToRemove->m_prev != nullptr);
-
-		Cup* nextCup = cupToRemove->m_next;
-		Cup* prevCup = cupToRemove->m_prev;
-
-		ConnectCup(prevCup, nextCup);
-
-		cupToRemove->m_next = nullptr;
-		cupToRemove->m_prev = nullptr;
-	}
-
 	void DoMove(Cup* currentCup)
 	{
 		PrintCups(currentCup);
 
 		// The crab picks up the three cups that are immediately clockwise of the current cup.
 		// They are removed from the circle; cup spacing is adjusted as necessary to maintain the circle.
+		Cup* cup1 = currentCup->m_next;
+		Cup* cup2 = cup1->m_next;
+		Cup* cup3 = cup2->m_next;
+
 		vector<Cup*> pickUps;
-		const int c_numPickupCups = 3;
-		for (int i = 0; i < c_numPickupCups; ++i)
-		{
-			Cup* nextCup = currentCup->m_next;
-			pickUps.push_back(nextCup);
-			RemoveCup(nextCup);
-		}
+		pickUps.push_back(cup1);
+		pickUps.push_back(cup2);
+		pickUps.push_back(cup3);
+
+		// Fill in gap where chain of cups has been removed
+		currentCup->m_next = cup3->m_next;
 
 #ifdef DEBUG_PRINT
 		cout << endl << "pick up:  ";
@@ -133,6 +112,12 @@ public:
 		// If this would select one of the cups that was just picked up, the crab will keep subtracting one until 
 		// it finds a cup that wasn't just picked up.
 		int destValue = currentCup->m_label - 1;
+		
+		// If at any point in this process the value goes below the lowest value on any cup's label, 
+		// it wraps around to the highest value on any cup's label instead.
+		if (destValue < m_lowestLabel)
+			destValue = m_highestLabel;
+
 		bool foundDestValue = false;
 		while (!foundDestValue)
 		{
@@ -154,31 +139,20 @@ public:
 			}
 		}
 
-		Cup* destinationCup = nullptr;
-		Cup* checkedCup = currentCup->m_next;
-		while (destinationCup == nullptr)
-		{
-			if (checkedCup->m_label == destValue)
-			{
-				destinationCup = checkedCup;
-			}
-			checkedCup = checkedCup->m_next;
-		}
-
+		Cup* destinationCup = m_allCups[destValue];
 #ifdef DEBUG_PRINT
 		cout << endl << "destination:\t" << destinationCup->m_label << endl << endl;
 #endif
 
 		// The crab places the cups it just picked up so that they are immediately clockwise of the destination cup. 
 		// They keep the same order as when they were picked up.
-		InsertCup(pickUps[0], destinationCup);
-		InsertCup(pickUps[1], pickUps[0]);
-		InsertCup(pickUps[2], pickUps[1]);
+		pickUps[2]->m_next = destinationCup->m_next;
+		destinationCup->m_next = pickUps[0];
 	}
 
 	void PlayGame(int maxMoves)
 	{
-		Cup* currentCup = m_allCups.front();
+		Cup* currentCup = m_firstCup;
 		int moveNumber = 1;
 		while (moveNumber <= maxMoves)
 		{
@@ -199,33 +173,50 @@ public:
 
 	Cup* FindCupByLabel(int label)
 	{
-		for (Cup* cup : m_allCups)
-		{
-			if (cup->m_label == label)
-				return cup;
-		}
-		return nullptr;
+		return m_allCups[label];
 	}
 
 	void ExpandToCupCount(int cupCount) 
 	{
-		cupCount -= m_allCups.size();
-		while (cupCount > 0)
+		while (m_numCups < cupCount)
 		{
 			Cup* newCup = new Cup();
 			m_highestLabel++;
 			newCup->m_label = m_highestLabel;
 
-			InsertCup(newCup, m_allCups.back());
+			m_lastCup->m_next = newCup;
 			m_allCups.push_back(newCup);
-
-			cupCount--;
+			m_lastCup = newCup;
+			m_numCups++;
 		}
+
+		m_lastCup->m_next = m_firstCup;
+	}
+
+	bool IsValidLoop(int& finalLabel) const 
+	{
+		int cupCount = 0;
+		Cup* cup = m_firstCup;
+		while (cupCount <= m_numCups)
+		{
+			if (cup == m_firstCup && cupCount == m_numCups)
+			{
+				finalLabel = cup->m_label;
+				return true;
+			}
+			cup = cup->m_next;
+			cupCount++;
+		}
+		finalLabel = cup->m_label;
+		return false;
 	}
 
 private:
-	vector<Cup*> m_allCups;
+	vector<Cup*> m_allCups; // Use label as index, to find cup in O(1) time
+	Cup* m_firstCup;
+	Cup* m_lastCup;
 
+	int m_numCups;
 	int m_highestLabel;
 	int m_lowestLabel;
 };
@@ -248,8 +239,8 @@ string GetAnswerString(Cup* fromCup)
 
 int main()
 {
-	string cupInfo = c_testInput;
-	//string cupInfo = c_input;
+	//string cupInfo = c_testInput;
+	string cupInfo = c_input;
 
 	{
 		// Part One
@@ -260,6 +251,10 @@ int main()
 		game.PlayGame(maxMoves);
 
 		cout << "The labels on the cups after cup 1 is: " << GetAnswerString(game.FindCupByLabel(1)) << endl;
+
+		int finalLabel;
+		bool isValid = game.IsValidLoop(finalLabel);
+		cout << "Valid loop: " << isValid << " - " << finalLabel << endl;
 	}
 
 	{
@@ -267,12 +262,21 @@ int main()
 		cout << endl << "=== Part 2 ===" << endl;
 
 		CupGame game(cupInfo);
-		const int maxMoves = 10000000; // Ten million moves!
 		const int cupCount = 1000000; // One million cups!
 		game.ExpandToCupCount(cupCount);
+		const int maxMoves = 10000000; // Ten million moves!
 		game.PlayGame(maxMoves);
 
-		int i = 0;
+		Cup* cup1 = game.FindCupByLabel(1);
+		uint64_t neighbourOneLabel = cup1->m_next->m_label;
+		uint64_t neighbourTwoLabel = cup1->m_next->m_next->m_label;
+		cout << "The immediate cup labels are: " << neighbourOneLabel << ", " << neighbourTwoLabel << endl;
+		uint64_t neighbourProduct = (neighbourOneLabel * neighbourTwoLabel);
+		cout << "The product is: " << neighbourProduct << endl;
+
+		int finalLabel;
+		bool isValid = game.IsValidLoop(finalLabel);
+		cout << "Valid loop: " << isValid << " - " << finalLabel << endl;
 	}
 
 }
